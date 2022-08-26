5e465b5da921481a2a523df2fd7686bf85104462
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-08-26 07:23:50 -0300

    Update script.js

--- Javascript/aula56-factory_functions/script.js
+++ Javascript/aula56-factory_functions/script.js
@@ -1,24 +1,39 @@
-function criaPessoa(name, lastName, weight, height) {
+function criaPessoa(firstName, lastName, weight, height) {
     return {
-        name, 
+        firstName, 
         lastName,
         weight,
         height,
         talk(subject) {
-            return `${name} is talking about ${subject}`;
-            //"this" is refering the object that called it
+            return `${this.firstName} is talking about ${subject}`;
+            //**1: "this" is refering the object that called it
         },
-        imc() {
+
+        get imc() { //**2: with "get" in front of the method it's not necessary to call the method with parentheses to trigger it (like an attribute)
             const index = this.weight / (this.height**2);
             return index.toFixed(2);
-        }
+        },
+
+        get fullName() {
+            return `${this.firstName} ${this.lastName}`
+        },
+
+        set fullName(fullName) { //**3: "set" can hold any value of any data type
+            splitFullName = fullName.split(' ');
+            this.firstName = splitFullName.shift()
+            this.lastName = splitFullName.join(' ')
+        },
     }
 }
 
 const p1 = criaPessoa('Toshio', 'Yasunaka', 65, 1.74);
+p1.fullName = 'Walter Toshio Yasunaka'
 console.log(p1.talk('JS'));
-console.log(p1.imc())
+console.log(p1.imc) //**2: with "get" in front of the method it's not necessary to call the method with parentheses to trigger it (like an attribute)
+console.log(p1.fullName)
 
 const p2 = criaPessoa('Walter', 'White', 80, 1.80);
+p2.fullName = 'Walter White Careca'
 console.log(p2.talk('methamphetamine'));
-console.log(p2.imc())
+console.log(p2.imc) 
+console.log(p2.fullName)
