2abbd26bb6d4f7df773278d1f8c46403aa7a36c4
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-09-23 06:39:16 -0300

    New: exercise 41

--- Javascript/aula41-exercise_logic01/assets/js/main.js
+++ Javascript/aula41-exercise_logic01/assets/js/main.js
@@ -3,16 +3,23 @@ const container = document.querySelector('.container');
 function calc(event) {
     event.preventDefault();
 
-    const firstNumber = document.querySelector('#number-1').value;
-    const secondNumber = document.querySelector('#number-2').value;
+    const firstNumber = Number(document.querySelector('#number-1').value);
+    const secondNumber = Number(document.querySelector('#number-2').value);
 
     const createdDiv = document.createElement('p');
+    createdDiv.id = 'result';
+
     const divContent = 
         firstNumber > secondNumber ? 'O número ' + firstNumber + ' é maior.' : 
         firstNumber == secondNumber ? 'Ambos os números tem o mesmo valor' : 'O número ' + secondNumber + ' é maior.';
 
-    createdDiv.innerText = divContent;
-    container.appendChild(createdDiv);
+    let hasResult = document.querySelector('#result');
+    if (hasResult) {
+        hasResult.innerHTML = divContent;
+    } else {
+        createdDiv.innerText = divContent;
+        container.appendChild(createdDiv);
+    }
 
     document.querySelector('#number-1').value = '';
     document.querySelector('#number-2').value = '';
