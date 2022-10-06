c9e850eb67a4d27f40c7383a59290649028b3bc0
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-10-06 10:23:55 -0300

    New: class 76 - Prototype_heritage

--- Javascript/aula76-objects_heritage/script.js
+++ Javascript/aula76-objects_heritage/script.js
@@ -1,15 +1,72 @@
-function Pessoa(name, lastname) {
-  this.name = name;
-  this.lastname = lastname;
-  // this.fullname = () => this.name + ' ' + this.lastname; //now, the method below will be used Pessoa.prototype.fullname
+// Product -> increase, discount;
+// T-shirt -> color;
+// Mug -> material;
+
+
+// **** PRODUCT ****
+function Product(name, price) {
+  this.name = name,
+  this.price = price
 }
 
-Pessoa.prototype.fullname = function() {
-  return this.name + ' ' + this.lastname;
-
+Product.prototype.increase = function(percentage) {
+  this.price = this.price + this.price * (percentage / 100);
 }
 
-const pessoa1 = new Pessoa('Toshio', 'Yasunaka');
-const pessoa2 = new Pessoa('Walter', 'Yasunaka');
+Product.prototype.discount = function(percentage) {
+  this.price = this.price - this.price * (percentage / 100);
+}
 
-console.log(pessoa1);
+const prod = new Product('Any product', 300);
+
+
+// **** T-SHIRT ****
+function TShirt(name, price, color) {
+  Product.call(this, name, price); // by heritage, the properties of Product were passed to Tshirt;
+  this.color = color; ///In addition to the property add "color";
+}
+
+// to add "increase" and "discount" in TShirt we pass the prototypes from Product to TShirt
+TShirt.prototype = Object.create(Product.prototype); // as the prototype of "products" were allocated, TShirt lost its prototype.constructor
+TShirt.prototype.constructor = TShirt; // so here we just put the constructor back
+
+TShirt.prototype.increase = function(increaseValue) { //here we changed prototype method "increase"
+  this.price += increaseValue;
+}
+
+const t1 = new TShirt('tshirt1', 200, 'black');
+t1.increase(50);
+
+
+
+// **** MUG ****
+function Mug(name, price, material, storage) {
+  Product.call(this, name, price),
+  this.material = material;
+  
+  Object.defineProperty(this, 'storage', {
+    enumerable: true,
+    configurable: false,
+
+    get: function() {
+      return storage;
+    },
+
+    set: function(value) {
+      if(typeof value !== 'number') return;
+      storage = value;
+    }
+  })
+}
+
+Mug.prototype = Object.create(Product.prototype);
+Mug.prototype.constructor = Mug;
+
+const mug1 = new Mug('One Piece Mug', 20, 'porcelain', 300);
+mug1.discount(50);
+mug1.storage = 100;
+
+
+console.log(prod);
+console.log(t1);
+console.log(mug1)
