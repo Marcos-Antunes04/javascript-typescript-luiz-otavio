14bcce113e08c8d297db33cc949cb3e3e120d835
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-10-12 11:12:25 -0300

    New: class 84 - Cpf exercise with classes

--- Javascript/aula84-CPF_exercise_class/script.js
+++ Javascript/aula84-CPF_exercise_class/script.js
@@ -1,56 +1,55 @@
-function isSequence(cleanCpf) {
-  const sequence = cleanCpf[0].repeat(cleanCpf.length);
-  return sequence === cleanCpf;
-}
-
-function firstValidations(cleanCpf) {
-  if (cleanCpf === 'undefined') return false;
-  if (cleanCpf.length !== 11) return false;
-  if (isSequence(cleanCpf)) return false;
-  return true;
-}
-
-function calculateNumbers(numbersToCalculate) {
-  let calculatedNumbers = [];
-  
-  for (let index in numbersToCalculate) {
-    calculatedNumbers.push(numbersToCalculate[index] * ((numbersToCalculate.length + 1) - index))
+class ValidateCpf {
+  constructor(cpf) {
+    Object.defineProperty(this, 'cleanCpf', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpf.replace(/\D+/g, '')
+    });
   }
 
-  return calculatedNumbers;
-}
+  //static method
+  static generateDigit(partOfCpf) {
+    let total = 0;
+    let counter = partOfCpf.length + 1;
 
-function getCalculatedNumbersSum(calculatedNumbers) {
-  return calculatedNumbers.reduce((accumulator, number) => accumulator += Number(number), 0)
-}
-
-function getNewNumber(number) {
-  let newNumber = 11 - (number % 11);
-  return newNumber > 9 ? 0 : newNumber;
-}
-
-function validateCpf(cpf) {
-  const cleanCpf = cpf.replace(/\D+/g, '');
-  if(firstValidations(cleanCpf)) {
-    const cpfArray = cleanCpf.split('');
-    //const cpfArray = Array.from(cleanCpf);  //"Array.from()" build a new array;
-    let numbersToCalculate = cpfArray.slice(0, 9);
-    let calculatedNumbers = calculateNumbers(numbersToCalculate);
-    let calculatedNumbersSum = getCalculatedNumbersSum(calculatedNumbers);
-    const tenthNumber = getNewNumber(calculatedNumbersSum);
-  
-    if (cpfArray[9] == tenthNumber) {
-      numbersToCalculate.push(tenthNumber);
-    
-      calculatedNumbers = calculateNumbers(numbersToCalculate);
-      calculatedNumbersSum = getCalculatedNumbersSum(calculatedNumbers);
-      
-      const tenthfirstNumber = getNewNumber(calculatedNumbersSum);
-      return tenthfirstNumber == cpfArray[10];
+    for(let digit of partOfCpf) {
+      total += Number(digit) * counter;
+      counter--;
     }
-    return false;
+
+    const newDigit = 11 - (total % 11);
+    return newDigit >= 10 ? 0 : newDigit;
   }
-  return false;
+  
+  isSequence() {
+    return this.cleanCpf.charAt(0).repeat(this.cleanCpf.length) === this.cleanCpf;
+  }
+
+  createNewCpf() {
+    const partOfCpf = this.cleanCpf.slice(0, -2);
+    const digit10 = ValidateCpf.generateDigit(partOfCpf); //calls static method
+    const digit11 = ValidateCpf.generateDigit(partOfCpf + digit10); //calls static method
+
+    return partOfCpf + '' + digit10 + '' + digit11;
+  }
+
+  validate() {
+    if (!this.cleanCpf) return false;
+    if (typeof this.cleanCpf !== 'string') return false;
+    if (this.cleanCpf.length !== 11) return false;
+    if (this.isSequence()) return false;
+    const newCpf = this.createNewCpf();
+
+    return Number(newCpf) === Number(this.cleanCpf);
+  }
 }
 
-console.log(validateCpf('705.484.450-52'));
+// 070.987.720-03     705.484.450-52
+let validateCpf = new ValidateCpf('070.987.720-03');
+
+if (validateCpf.validate()) {
+  console.log('CPF válido');
+} else {
+  console.log('CPF inválido');
+}
