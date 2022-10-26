fa9e9a383dd55742c8bacc54d96f2c433380ed65
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-10-26 07:28:36 -0300

    New: class node 10 - views

--- Javascript/node/class10-express_views/routes.js
+++ Javascript/node/class10-express_views/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const route = express.Router();
-const homeController = require('./controllers/homeController');
-const contactController = require('./controllers/contactController');
-const aboutController = require('./controllers/aboutController');
+const homeController = require('./src/controllers/homeController');
+const contactController = require('./src/controllers/contactController');
+const aboutController = require('./src/controllers/aboutController');
 
 // Home routes
 route.get('/', homeController.initialPage);
