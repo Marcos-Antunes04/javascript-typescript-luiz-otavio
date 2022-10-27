7babac46b36c8bda5ec478d678436be4b758a364
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-10-27 11:09:45 -0300

    New: class 14 - connection and first model

---
+++ Javascript/node/class14-connection_and_first_model/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Connect database');
+        app.emit('ready');
+    })
+    .catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { globalMiddleware, anotherMiddleware } = require('./src/middleware/middleware');
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+// app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+//Our own middlewares
+app.use(globalMiddleware);
+app.use(anotherMiddleware);
+app.use(routes);
+
+app.on('ready', () => {
+    app.listen(3000, () => {
+        console.log('Access http://localhost:3000');
+        console.log('Server running on port 3000.');
+    });
+})
