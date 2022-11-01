94e308b62c0a60b146aacbed78399d23da474819
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-11-01 10:09:58 -0300

    New: customer base

--- Javascript/projects/contacts/src/middleware/middleware.js
+++ Javascript/projects/contacts/src/middleware/middleware.js
@@ -17,3 +17,12 @@ exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 }
+
+exports.loginRequired = (req, res, next) => {
+    if(!req.session.user) {
+        req.flash('errors', 'You need to log in.');
+        req.session.save(() => res.redirect('/'));
+        return;
+    }
+    next();
+}
---
+++ Javascript/projects/contacts/src/models/ContactModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ContactSchema = new mongoose.Schema({
+    firstname: { type: String, required: false },
+    lastname: { type: String, required: false, default: '' },
+    email: { type: String, required: false, default: '' },
+    cellphone: { type: String, required: false, default: '' },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const ContactModel = mongoose.model('Contact', ContactSchema);
+
+function Contact(body) {
+    this.body = body;
+    this.errors = [];
+    this.contact = null;
+}
+
+Contact.searchById = async function(id) {
+    if(typeof id !== 'string') return;
+    const user = await ContactModel.findById(id);
+    return user;
+}
+
+Contact.prototype.register = async function() {
+    this.validate();
+    if(this.errors.lenght > 0) return;
+    this.contact = await ContactModel.create(this.body);
+}
+
+Contact.prototype.validate = function() {
+    this.cleanUp();
+    // The e-mail must be valid
+    if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('Invalid e-mail.');
+    if(!this.body.firstname) this.errors.push('Firstname is required');
+    if(!this.body.email && !this.body.cellphone) this.errors.push('E-mail or phone number is required');
+}
+
+Contact.prototype.cleanUp = function() {
+    for(const key in this.body) {
+        if(typeof this.body[key] !== 'string') {
+            this.body[key] = ';'
+        };
+    }
+
+    this.body = {
+        firstname: this.body.firstname,
+        lastname: this.body.lastname,
+        email: this.body.email,
+        cellphone: this.body.cellphone,
+    }
+}
+
+
+module.exports = Contact;
