318012569ed49f964e4e3113743862d6a060118c
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2022-11-14 11:07:11 -0300

    Create: form css

--- React/tasklist/src/components/Main.js
+++ React/tasklist/src/components/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Main.css'
 
+// Form
+import { FaPlus } from 'react-icons/fa'
+
 export default class Main extends Component {
   state = {
     newTask: '',
@@ -19,9 +22,15 @@ export default class Main extends Component {
       <div className='main'>
         <h1>Tasklist</h1>
 
-        <form action="#">
-          <input onChange={this.handleInputChange} type="text" />
-          <button type="submit">Send</button>
+        <form action="#" className='form'>
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            value={newTask}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
 
         <p> {newTask} </p>
