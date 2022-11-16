540ba218cdeefe6fe7679ad9ce5e4e0d588d62b9
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2022-11-16 16:27:25 -0300

    New: forms style

--- React/tasklist/src/components/Main.js
+++ React/tasklist/src/components/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Main.css'
 
+// Form
+import { FaPlus } from 'react-icons/fa'
+
 export default class Main extends Component {
   state = {
     newTask: '',
@@ -13,15 +16,18 @@ export default class Main extends Component {
   }
 
   render() {
-    // const { newTask } = this.state
+    const { newTask } = this.state
 
     return (
       <div className="main">
         <h1>Tasklist</h1>
 
-        <form action="#">
-          <input onChange={this.handleInputChange} type="text" />
-          <button type="submit">Send</button>
+        <form className='form' action="#">
+          <input onChange={this.handleInputChange} type="text" value={newTask} />
+
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     )
